7c218f60a8ade89e11ef06f536841cacafed1e12
Cryptxnaut <jaiakilesh06@gmail.com>
2023-08-08 10:23:32 +0100

    Coordinate pairs using <vector>

--- src/Pure-Pursuit.cpp
+++ src/Pure-Pursuit.cpp
@@ -10,13 +10,27 @@ using namespace std;
 #include <stdlib.h>
 #include <iostream>
 #include <vector>
-
+#include <utility>
 
 
 
 void PurePursuit(){
     PurePursuitClass PurePursuit;
     OdometryClass Odometry;
+
+    std::vector<std::pair<double, double>> coordinatePairs;
+
+    coordinatePairs.push_back(std::make_pair(1.0, 2.0));
+    coordinatePairs.push_back(std::make_pair(3.0, 4.0));
+    coordinatePairs.push_back(std::make_pair(5.0, 6.0));
+
+    if(!coordinatePairs.empty()){
+        const auto& lastPair = coordinatePairs.back();
+        std::cout << "Last Coordinates: (" << lastPair.first << ", " << lastPair.second << ")" << std::endl;
+    }
+    else{
+        std::cout << "No coordinates" << std::endl;
+    }
 
     
 
