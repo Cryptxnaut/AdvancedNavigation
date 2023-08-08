468c3736d5e991c9bc1c8abfcaa8cc3cd5f8bec0
Cryptxnaut <jaiakilesh06@gmail.com>
2023-08-08 20:17:40 +0100

    finding closest lookahead point

--- src/Pure-Pursuit.cpp
+++ src/Pure-Pursuit.cpp
@@ -15,7 +15,7 @@ using namespace std;
 #include <utility>
 
 
-
+//const std::vector<wayPoints>& path, const robotState& robot
 void PurePursuit(){
     PurePursuitClass PurePursuit;
     OdometryClass Odometry;
@@ -38,12 +38,25 @@ void PurePursuit(){
         Odometry.DeltaTheta
     };
 
-
-    
+    wayPoints finalPoint = path.back();    
 
 
     for(int i = 0; i < path.size(); i++){
         wayPoints lookaheadPoint = getLookaheadPoint(robot, path);
+
+        int closestPointIndex = 0;
+        double minDistance = distance(lookaheadPoint.x, lookaheadPoint.y, path[0].x, path[0].y);
+
+        for(int i = 1; i < path.size(); i++){
+            double d = distance(lookaheadPoint.x, lookaheadPoint.y, path[i].x, path[i].y);
+            if(d < minDistance){
+                minDistance = d;
+                closestPointIndex = i;
+            }
+        }
+
+        double t = static_cast<double>(closestPointIndex) / static_cast<double>(path.size() - 1);
+
 
         double dx = lookaheadPoint.x - robot.x;
         double dy = lookaheadPoint.y - robot.y;
